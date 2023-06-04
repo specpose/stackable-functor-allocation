8920765d9d57f5cbda0bb2a470388cbfcd21c872
Fassio Blatter <fassio@specpose.com>
2023-06-04 18:59:59 +0100

    ADDED SFA::Lazy, SFA::Strict

--- include/stackable-functor-allocation/Thread.hpp
+++ include/stackable-functor-allocation/Thread.hpp
@@ -1,48 +1,12 @@
 #pragma once
-#include "stackable-functor-allocation/error.h"
-#include <thread>
-#include <functional>
 
 namespace SFA {
-    template<typename T> class Thread {
-        public:
-	    Thread(T functor) : functorCopy(functor){
-            if (_thread){
-#if DEBUG
-                throw SFA::util::runtime_error("Constructor: Thread is a leftover thread",__FILE__,__func__);
-#endif
-            } else {
-                _running=true;
-                _thread = new std::thread( std::mem_fn(&T::operator()),functorCopy );
-            }
-        };
-        virtual ~Thread() {
-            if (_thread) {
-#if DEBUG
-                throw SFA::util::runtime_error("Destructor: Thread is still running",__FILE__,__func__);
-#endif
-                this->join();
-            }
-        };
-        virtual void operator()() final{
-            this->join();
-        };
-        private:
-        void join(){
-            if (_thread){
-                if (_thread->joinable()){
-                    _thread->join();
-                }
-#if DEBUG
-                if (_running || _thread->joinable())//still running?
-				    throw SFA::util::runtime_error("SuperClass has not set running to false",__FILE__,__func__);
-#endif
-			    delete _thread;
-			    _thread = nullptr;
-            }
-        };
-        bool _running=false;
-        std::thread* _thread=nullptr;
-        T functorCopy;
+    struct Strict {
+        protected:
+        virtual void constexpr operator()()=0;
+    };
+    struct Lazy {
+        protected:
+        virtual void operator()()=0;
     };
 }
--- test/Thread.cpp
+++ test/Thread.cpp
@@ -1,18 +1,5 @@
 #include "stackable-functor-allocation/Thread.hpp"
-#include <iostream>
-
-class MyFunctor {
-    public:
-    void operator()(){
-        std::cout << message <<std::endl;
-    }
-    private:
-    std::string message = std::string{"Thread is running."};
-};
 
 int main()
 {
-    auto data = MyFunctor();
-    auto t1 = SFA::Thread<MyFunctor>(data);
-    t1();
 }
