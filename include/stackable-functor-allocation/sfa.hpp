cd0c282008c26f567802a69eb01ad5d81b2634f9
Fassio Blatter <fassio@specpose.com>
2023-12-15 16:15:57 +0000

    FIXED base class visibility

--- include/stackable-functor-allocation/sfa.hpp
+++ include/stackable-functor-allocation/sfa.hpp
@@ -10,18 +10,18 @@ namespace NonV {
     };
 }
 namespace SFA {
-    template<typename T, std::size_t N, std::size_t O=N> class Strict : private std::array<T,N> {
+    template<typename T, std::size_t N, std::size_t O=N> class Strict : public std::array<T,N> {
         public:
         Strict(const std::array<T,O>& inputBuffer) : _input(inputBuffer) {}
-        virtual void operator()() const=0;
+        virtual void operator()() const=0;//nonstatic!
 	virtual std::size_t constexpr size() = 0;
         protected:
         std::array<T,O> _input;
     };
-    template<typename T> class Lazy : private std::vector<T> {
+    template<typename T> class Lazy : public std::vector<T> {
         public:
         Lazy(const std::vector<T> & inputBuffer) : _input(inputBuffer){}
-        virtual void operator()() const=0;
+        virtual void operator()() const=0;//nonstatic
 	virtual std::size_t size() = 0;
         protected:
         std::vector<T> _input;
