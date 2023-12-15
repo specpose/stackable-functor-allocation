2df424928be744123ec0f832544808243d145ebc
Fassio Blatter <fassio@specpose.com>
2023-12-15 15:16:21 +0000

    REFACTORING size calculation

--- include/stackable-functor-allocation/sfa.hpp
+++ include/stackable-functor-allocation/sfa.hpp
@@ -10,15 +10,21 @@ namespace NonV {
     };
 }
 namespace SFA {
-    template<typename T, std::size_t N> class Strict : public std::array<T,N> {
+    template<typename T, std::size_t N, std::size_t O=N> class Strict : private std::array<T,N> {
         public:
+        Strict(const std::array<T,O>& inputBuffer) : _input(inputBuffer) {}
         virtual void operator()() const=0;
-	virtual std::size_t constexpr size() final {return N;};
+	virtual std::size_t constexpr size() = 0;
+        protected:
+        std::array<T,O> _input;
     };
-    template<typename T> class Lazy : public std::vector<T> {
+    template<typename T> class Lazy : private std::vector<T> {
         public:
+        Lazy(const std::vector<T> & inputBuffer) : _input(inputBuffer){}
         virtual void operator()() const=0;
-	virtual std::size_t size() final {return this->size();};
+	virtual std::size_t size() = 0;
+        protected:
+        std::vector<T> _input;
     };
 }
 namespace STL {
--- test/sfa.cpp
+++ test/sfa.cpp
@@ -5,26 +5,30 @@ class ROMTest : public NonV::ROM {
     public:
     virtual void operator()() const final {
         std::cout << "Executing NonV::ROM..." << std::endl;
-        modifyMe = true;
+        //modifyMe = true;
     };
     private:
     bool modifyMe = false;
 };
 
-template<typename T, std::size_t HeapSize> class Buffer : public SFA::Strict<T,HeapSize> {
+template<typename T, std::size_t N, std::size_t O=N> class Buffer : public SFA::Strict<T,N,O> {
     public:
+    Buffer(const std::array<T,O>& inputBufferCopy) : SFA::Strict<T,N,O>(inputBufferCopy) {}
     virtual void operator()() const final {
         std::cout << "Executing SFA::Strict..." << std::endl;
-        this->at(0)=T{};
+        //this->at(0)=T{};
     };
+    virtual std::size_t constexpr size() final {return O-1;}
 };
 
 template<typename T> class FreeStore : public SFA::Lazy<T> {
     public:
+    FreeStore(const std::vector<T>& inputBufferCopy) : SFA::Lazy<T>(inputBufferCopy) {}
     virtual void operator()() const final {
         std::cout << "Executing SFA::Lazy..." << std::endl;
-        this->push_back(T{});
+        //this->push_back(T{});
     };
+    virtual std::size_t size() final {return SFA::Lazy<T>::_input.size()-1;};
 };
 
 using data_type = int;
@@ -41,14 +45,15 @@ template<> void STL::function<ANamedBufferType> (
 
 int main()
 {
-    const std::size_t heap_size = 1;
+    const std::size_t input_length = 2;
     auto nonv = ROMTest{};
     nonv();
-    auto strict = Buffer<data_type, heap_size>{};
+    auto inputArray = std::array<data_type,input_length>{};
+    auto strict = Buffer<data_type, input_length>(inputArray);
     strict();
-    auto lazy = FreeStore<data_type>{};
+    auto inputVector = std::vector<data_type>{};
+    auto lazy = FreeStore<data_type>(inputVector);
     lazy();
-    auto inputBuffer = std::vector<data_type>{};
     auto outputBuffer = ANamedBufferType{};
-    STL::function<decltype(outputBuffer)>(inputBuffer.begin(),inputBuffer.end(),outputBuffer.begin());
+    STL::function<decltype(outputBuffer)>(inputVector.begin(),inputVector.end(),outputBuffer.begin());
 }
