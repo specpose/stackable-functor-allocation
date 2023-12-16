40fc9436e7ab9470f5afa9079113c153120f7a2d
Fassio Blatter <fassio@specpose.com>
2023-12-16 00:18:37 +0000

    FIXED StackableFunctor

--- include/stackable-functor-allocation/sfa.hpp
+++ include/stackable-functor-allocation/sfa.hpp
@@ -3,14 +3,22 @@
 #include <vector>
 #include <functional>
 namespace STL {
-    template<typename InputBufferType, typename OutputBufferType> void transform(
+    template<typename InputBufferType, typename OutputBufferType> void transform(//Refactoring 1
         typename InputBufferType::iterator inputStart,
         typename InputBufferType::iterator InputEnd,
         typename OutputBufferType::iterator OutputStart
         );
 }
 namespace SFA {
-    template<typename T, std::size_t N, std::size_t O=N> class Strict : public std::array<T,N> {
+    template<typename T> class Lazy : public std::vector<T> {//Refactoring 2
+        public:
+        Lazy(const std::vector<T> & inputBuffer) : _input(inputBuffer), std::vector<T>{} {}
+        virtual void operator()() const=0;//nonstatic!
+	virtual std::size_t size() = 0;
+        protected:
+        std::vector<T> _input;
+    };
+    template<typename T, std::size_t N, std::size_t O=N> class Strict : public std::array<T,N> {//Refactoring 3
         public:
         Strict(const std::array<T,O>& inputBuffer) : _input(inputBuffer), std::array<T,N>{} {}
         virtual void operator()() const=0;//nonstatic!
@@ -18,20 +26,12 @@ namespace SFA {
         protected:
         std::array<T,O> _input;
     };
-    template<typename T> class Lazy : public std::vector<T> {
-        public:
-        Lazy(const std::vector<T> & inputBuffer) : _input(inputBuffer), std::vector<T>{} {}
-        virtual void operator()() const=0;//nonstatic
-	virtual std::size_t size() = 0;
-        protected:
-        std::vector<T> _input;
-    };
 }
 namespace NonV {
-    template<typename T, std::size_t N, typename Previous> class StackableFunctor : public virtual std::array<T,N> {
+    template<typename T, std::size_t N, typename Previous> class StackableFunctor : public std::array<T,N> {//Refactoring 4
         public:
         StackableFunctor(Previous& prev) : _previous(prev), std::array<T,N>{} {}
-        virtual void operator()() const=0;//nonstatic
+        virtual void operator()() const=0;//nonstatic!
         virtual std::size_t constexpr size() = 0;
         protected:
         Previous& _previous;
--- test/sfa.cpp
+++ test/sfa.cpp
@@ -1,14 +1,26 @@
 #include "stackable-functor-allocation/sfa.hpp"
 #include <iostream>
 
-template<typename T, std::size_t N, typename Previous> class SFATest : public NonV::StackableFunctor<T,N,Previous> {
+template<typename T, std::size_t N, typename Previous> class SFAItem : public NonV::StackableFunctor<T,N,Previous> {
     public:
-    SFATest(Previous& prev) : NonV::StackableFunctor<T,N,Previous>(prev) {}
+    SFAItem(Previous& prev) : NonV::StackableFunctor<T,N,Previous>(prev) {}
+    virtual void operator()() const final {
+        NonV::StackableFunctor<T,N,Previous>::_previous();
+        std::cout << "Executing NonV::ROM..." << std::endl;
+        //modifyMe = true;
+    };
+    virtual std::size_t constexpr size() final {return NonV::StackableFunctor<T,N,Previous>::_previous.size()-1;}//size of Previous?
+    private:
+    bool modifyMe = false;
+};
+template<typename T, std::size_t N, std::size_t O> class SFAFirst : public NonV::StackableFunctor<T,N,std::array<T,O>> {
+    public:
+    SFAFirst(std::array<T,O>& prev) : NonV::StackableFunctor<T,N,std::array<T,O>>(prev) {}
     virtual void operator()() const final {
         std::cout << "Executing NonV::ROM..." << std::endl;
         //modifyMe = true;
     };
-    virtual std::size_t constexpr size() final {return 2-1;}//size of Previous?
+    virtual std::size_t constexpr size() final {return O-1;}//size of Previous?
     private:
     bool modifyMe = false;
 };
@@ -19,7 +31,7 @@ template<typename T, std::size_t N, std::size_t O=N> class Buffer : public SFA::
         std::cout << "Executing SFA::Strict..." << std::endl;
         //this->at(0)=T{};
     };
-    virtual std::size_t constexpr size() final {return O-1;}
+    virtual std::size_t constexpr size() final {return O-2;}
 };
 
 template<typename T> class FreeStore : public SFA::Lazy<T> {
@@ -29,7 +41,7 @@ template<typename T> class FreeStore : public SFA::Lazy<T> {
         std::cout << "Executing SFA::Lazy..." << std::endl;
         //this->push_back(T{});
     };
-    virtual std::size_t size() final {return SFA::Lazy<T>::_input.size()-1;};
+    virtual std::size_t size() final {return SFA::Lazy<T>::_input.size()-2;};
 };
 
 using data_type = int;
@@ -46,15 +58,28 @@ template<typename InputBufferType, typename OutputBufferType> void STL::transfor
 
 int main()
 {
-    const std::size_t input_length = 2;
+    const std::size_t input_length = 3;
     auto inputArray = std::array<data_type, input_length>{};
-    auto nonv = SFATest<data_type, input_length, decltype(inputArray)>(inputArray);
-    nonv();
-    auto strict = Buffer<data_type, input_length>(inputArray);
+    std::cout<<"Size of inputArray "<<input_length<<std::endl;
+    auto nonv = SFAFirst<data_type, input_length-1, input_length>(inputArray);
+    auto nonv2 = SFAItem<data_type, input_length-2, decltype(nonv)>(nonv);
+    nonv2();
+    std::cout<<"Size of nonv2 "<<nonv2.size()<<std::endl;
+    std::cout<<"Size of inputArray "<<input_length<<std::endl;
+    auto strict = Buffer<data_type, input_length-2, input_length>(inputArray);
     strict();
+    std::cout<<"Size of strict "<<strict.size()<<std::endl;
     auto inputVector = std::vector<data_type>{};
+    inputVector.push_back(data_type{});
+    inputVector.push_back(data_type{});
+    inputVector.push_back(data_type{});
+    std::cout<<"Size of inputVector "<<inputVector.size()<<std::endl;
     auto lazy = FreeStore<data_type>(inputVector);
     lazy();
+    std::cout<<"Size of lazy "<<lazy.size()<<std::endl;
     auto outputBuffer = std::vector<data_type>{};
+    outputBuffer.push_back(data_type{});
+    std::cout<<"Size of inputVector "<<inputVector.size()<<std::endl;
     STL::transform<decltype(inputVector),decltype(outputBuffer)>(inputVector.begin(),inputVector.end(),outputBuffer.begin());
+    std::cout<<"Size after STL::transform "<<outputBuffer.size()<<std::endl;
 }
