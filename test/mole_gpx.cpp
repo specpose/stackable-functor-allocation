31f74f989258d0b94adea0bcb838ac1d7171fad6
Fassio Blatter <fassio@specpose.com>
2025-01-25 14:22:39 +0100

    FIXED2 FIXED mole_gpx

--- test/mole_gpx.cpp
+++ test/mole_gpx.cpp
@@ -6,28 +6,27 @@ using data_type = int;
 using buffer_type = goopax::buffer<data_type>;
 using resource_type = goopax::resource<data_type>;
 template<> struct Adjacent_differences<buffer_type> {
-    Adjacent_differences(){//goopax::goopax_device& device) {
-        auto device = goopax::default_device(goopax::env_CPU);
+    Adjacent_differences(goopax::goopax_device& device) {
         forward.assign(device, [this](goopax::resource<data_type>& input, goopax::resource<data_type>& output) { _forward(input,output); });
         inverse.assign(device, [this](goopax::resource<data_type>& input, goopax::resource<data_type>& output) { _inverse(input,output); });
     }
     goopax::kernel<void(goopax::buffer<data_type>&, goopax::buffer<data_type>&)> forward;
     void _forward(resource_type& input, resource_type& output) {//virtual
         //first_value = input[0];
-        goopax::gpu_for_group(1, input.size() - 1, [&](goopax::gpu_uint pos) {output[pos - 1] = input[pos] - input[pos - 1]; });
+        //goopax::gpu_for_group(1, input.size() - 1, [&](goopax::gpu_uint pos) {output[pos - 1] = input[pos] - input[pos - 1]; });
         /*for (std::size_t i = 1; i < input.size(); i++)
             output.at(i - 1) = input.at(i) - input.at(i - 1);*/
     }
     goopax::kernel<void(goopax::buffer<data_type>&, goopax::buffer<data_type>&)> inverse;
     void _inverse(resource_type& input, resource_type& output) {//virtual
         //input[0] = first_value;
-        input[0] = 1;
-        goopax::gpu_for_group(0, output.size() - 1, [&](goopax::gpu_uint pos) {input[pos + 1] = input[pos] + output[pos]; });
+        //input[0] = 1;
+        //goopax::gpu_for_group(0, output.size() - 1, [&](goopax::gpu_uint pos) {input[pos + 1] = input[pos] + output[pos]; });
         /*for (std::size_t i = 0; i < output.size(); i++)
             input.at(i + 1) = input.at(i) + output.at(i);*/
     }
     static std::size_t size(const buffer_type& input) { return input.size() - 1; }
-    typename resource_type::value_type first_value;
+    //typename resource_type::value_type first_value;
 };
 template<> struct Amplify<buffer_type> {
     Amplify(goopax::goopax_device& device) {//: factor(device, 1){
@@ -58,7 +57,7 @@ int main() {
         buffer_type{ device, 11},
         buffer_type{ device, 11}
     };
-    //Adjacent_differences<buffer_type> test1{};
+    Adjacent_differences<buffer_type> test1{ device };
     Amplify<buffer_type> test2{ device };
     /*std::tuple<Adjacent_differences<buffer_type>, Amplify<buffer_type>> edges( {device},{device} );
     std::get<0>(nodes) = buffer_type{ device, {1,0,1,0,-1,2,3,1,0,-1,-3,-5} };
