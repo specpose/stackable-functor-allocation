cfa010d2c8eb11a84dfa4728b1ae0e121e2a0fa6
Fassio Blatter <fassio@specpose.com>
2025-01-26 12:30:03 +0100

    REFACTORING2 mole ParameterPack

--- test/mole_gpx.cpp
+++ test/mole_gpx.cpp
@@ -5,62 +5,64 @@
 using data_type = int;
 using buffer_type = goopax::buffer<data_type>;
 using resource_type = goopax::resource<data_type>;
+using params_type = std::tuple<data_type>;
 template<> struct Adjacent_differences<buffer_type> {
-    std::tuple<data_type> params{ 0 };
-    Adjacent_differences(goopax::goopax_device& device) : params_buf(device, 1) {
+    goopax::buffer<params_type> params{};
+    Adjacent_differences(goopax::goopax_device& device) : params(device, 1) {
         forward.assign(device, [this](goopax::resource<data_type>& input, goopax::resource<data_type>& output) {
-            goopax::resource<decltype(params)> v1(params_buf);
-            _forward(input,output,v1);
+            goopax::resource<params_type> p{params};
+            return _forward(input,output,p);
             });
         inverse.assign(device, [this](goopax::resource<data_type>& input, goopax::resource<data_type>& output) {
-            goopax::resource<decltype(params)> v1(params_buf);
-            _inverse(input,output,v1);
+            goopax::resource<params_type> p{params};
+            return _inverse(input,output,p);
             });
     }
     goopax::kernel<void(goopax::buffer<data_type>&, goopax::buffer<data_type>&)> forward;
-    static void _forward(resource_type& input, resource_type& output, goopax::resource<decltype(params)>& p) {
-        //first_value = input[0];
-        //goopax::gpu_for_group(1, input.size() - 1, [&](goopax::gpu_uint pos) {output[pos - 1] = input[pos] - input[pos - 1]; });
-        /*for (std::size_t i = 1; i < input.size(); i++)
-            output.at(i - 1) = input.at(i) - input.at(i - 1);*/
+    static void _forward(resource_type& input, resource_type& output, goopax::resource<params_type>& p) {
+        std::get<0>(p.front()) = input[0];
+        goopax::make_gpu<unsigned int>::type size = input.size();
+        for (unsigned int pos = 1; pos < 13; pos++)//size; pos++)
+        {
+            output[pos - 1] = input[pos] - input[pos - 1];
+        };
     }
     goopax::kernel<void(goopax::buffer<data_type>&, goopax::buffer<data_type>&)> inverse;
-    static void _inverse(resource_type& input, resource_type& output, goopax::resource<decltype(params)>& p) {
-        //input[0] = first_value;
-        //input[0] = 1;
-        //goopax::gpu_for_group(0, output.size() - 1, [&](goopax::gpu_uint pos) {input[pos + 1] = input[pos] + output[pos]; });
-        /*for (std::size_t i = 0; i < output.size(); i++)
-            input.at(i + 1) = input.at(i) + output.at(i);*/
+    static void _inverse(resource_type& input, resource_type& output, goopax::resource<params_type>& p) {
+        input[0] = std::get<0>(p.front());
+        for (unsigned int pos = 0; pos < 12; pos++)//Single ALU?
+        {
+            input[pos + 1] = input[pos] + output[pos];
+        };
     }
     static std::size_t size(const buffer_type& input) { return input.size() - 1; }
-    goopax::buffer<decltype(params)> params_buf;
 };
 template<> struct Amplify<buffer_type> {
-    std::tuple<data_type> params{ 0 };
-    Amplify(goopax::goopax_device& device, int s) : params_buf(device, 1) {
-        params_buf.fill({ s });
+    goopax::buffer<params_type> params{};
+    Amplify(goopax::goopax_device& device, int s) : params(device, 1) {
+        params = { params_type{s} };
         forward.assign(device, [this](goopax::resource<data_type>& input, goopax::resource<data_type>& output) {
-            goopax::resource<decltype(params)> p1(params_buf);
-            _forward(input, output, p1);
+            goopax::resource<params_type> v{ params };
+            return _forward(input, output, v);
             });
         inverse.assign(device, [this](goopax::resource<data_type>& input, goopax::resource<data_type>& output) {
-            goopax::resource<decltype(params)> p1(params_buf);
-            _inverse(input, output, p1);
+            goopax::resource<params_type> v{ params };
+            return _inverse(input, output, v);
             });
     }
     goopax::kernel<void(goopax::buffer<data_type>&, goopax::buffer<data_type>&)> forward;
-    static void _forward(resource_type& input, resource_type& output, goopax::resource<decltype(params)>& p) {
-        //std::transform(input.begin(), input.end(), output.begin(), [&](auto in) { return factor * in; });
+    static void _forward(resource_type& input, resource_type& output, goopax::resource<params_type>& v) {
+        goopax::make_gpu<std::size_t>::type factor = std::get<0>(v.front());
+        goopax::gpu_for_global(*output.begin(), *output.end(), [&input, &factor](goopax::make_gpu<unsigned int>::type i) { return factor * input[i]; });//std::size_t>::type i) { return factor * input[i]; });
     }
     goopax::kernel<void(goopax::buffer<data_type>&, goopax::buffer<data_type>&)> inverse;
-    static void _inverse(resource_type& input, resource_type& output, goopax::resource<decltype(params)>& p) {
-        //std::transform(output.begin(), output.end(), input.begin(), [&](auto out) { return out / factor; });
+    static void _inverse(resource_type& input, resource_type& output, goopax::resource<params_type>& v) {
+        goopax::gpu_for_global(*input.begin(), *input.end(), [&output, &v](goopax::make_gpu<unsigned int>::type i) { return output[i] / std::get<0>(v.front()); });
     }
     static std::size_t size(const buffer_type& input) { return input.size(); }
-    goopax::buffer<decltype(params)> params_buf;
 };
 std::ostream& operator<<(std::ostream& os, const buffer_type& _vec) {
-    auto vec = _vec.to_vector();
+    std::vector<buffer_type::value_type> vec = _vec.to_vector();
     std::for_each(vec.begin(), --vec.end(), [&](typename std::vector<data_type>::value_type e) {os << e << ","; });
     os << *(--vec.end());
     return os;
@@ -75,8 +77,16 @@ int main() {
     std::get<2>(nodes) = buffer_type( device, Amplify<buffer_type>::size(std::get<1>(nodes)));
     std::get<0>(edges).forward(std::get<0>(nodes), std::get<1>(nodes));
     std::get<0>(nodes).fill(0);
+    std::cout << "Afterfill: " << std::get<0>(nodes) << std::endl;
     std::get<1>(edges).forward(std::get<1>(nodes), std::get<2>(nodes));
-    //std::get<1>(edges).factor = 1;
+
+    //sync to GPU
+    std::vector<params_type> factor{ params_type{27} };
+    std::get<1>(edges).params = std::move(factor) ;
+    //std::cout << "Amplification factor after move assignment: " << std::get<0>(factor.front()) << std::endl;
+    //std::get<1>(edges).params.copy_to_host(&std::get<1>(edges).params);
+    std::cout << "Setting signal amplification factor to: " << std::get<0>(std::get<1>(edges).params.to_vector().front()) << std::endl;
+
     std::get<1>(edges).inverse(std::get<1>(nodes), std::get<2>(nodes));
     std::get<0>(edges).inverse(std::get<0>(nodes), std::get<1>(nodes));
     std::cout << "Reverted: " << std::get<0>(nodes) << std::endl;
