d29b651d6b6019021b43d91aa2e9c68b019e1085
Fassio Blatter <fassio@specpose.com>
2025-04-07 09:11:42 +0200

    Removed gpx first node

--- test/mole_gpx.cpp
+++ test/mole_gpx.cpp
@@ -68,23 +68,22 @@ std::ostream& operator<<(std::ostream& os, const std::vector<data_type> &vec) {
 }
 int main() {
     auto device = goopax::default_device(goopax::env_CPU);
-    std::tuple<buffer_type, buffer_type, buffer_type> nodes{ buffer_type{},buffer_type{},buffer_type{} };
+    std::tuple<buffer_type, buffer_type> nodes{ buffer_type{},buffer_type{} };
     std::tuple<Adjacent_differences<buffer_type>, Amplify<buffer_type>> edges{ Adjacent_differences<buffer_type>(device), Amplify<buffer_type>(device,2) };
-    const std::vector<data_type> inputBuffer{ 1,0,1,0,-1,2,3,1,0,-1,-3,-5 };
-    std::get<0>(nodes) = buffer_type{device, std::move(inputBuffer) };
-    std::cout << "Sink: " << std::get<0>(nodes).to_vector() << std::endl;
-    std::get<1>(nodes) = buffer_type( device, Adjacent_differences<buffer_type>::size(std::get<0>(nodes)));
-    std::get<2>(nodes) = buffer_type( device, Amplify<buffer_type>::size(std::get<1>(nodes)));
-    std::get<0>(edges).forward(std::get<0>(nodes), std::get<1>(nodes));
+    buffer_type inputData{ device, { 1,0,1,0,-1,2,3,1,0,-1,-3,-5 }};
+    std::cout << "Sink: " << inputData.to_vector() << std::endl;
+    std::get<0>(nodes) = buffer_type( device, Adjacent_differences<buffer_type>::size(inputData));
+    std::get<1>(nodes) = buffer_type( device, Amplify<buffer_type>::size(std::get<0>(nodes)));
+    std::get<0>(edges).forward(inputData, std::get<0>(nodes));
     std::vector<data_type> intermediateBuffer1(11);
-    std::get<1>(nodes).copy_to_host(intermediateBuffer1.data());
+    std::get<0>(nodes).copy_to_host(intermediateBuffer1.data());
     std::cout << "Jammed1: " << intermediateBuffer1 << std::endl;
-    std::get<1>(nodes) = std::move(intermediateBuffer1);
-    std::get<1>(edges).forward(std::get<1>(nodes), std::get<2>(nodes));
+    std::get<0>(nodes) = std::move(intermediateBuffer1);
+    std::get<1>(edges).forward(std::get<0>(nodes), std::get<1>(nodes));
     std::vector<data_type> endBuffer1(11);
-    std::get<2>(nodes).copy_to_host(endBuffer1.data());
+    std::get<1>(nodes).copy_to_host(endBuffer1.data());
     std::cout << "Source: " << endBuffer1 << std::endl;
-    std::get<2>(nodes) = std::move(endBuffer1);
+    std::get<1>(nodes) = std::move(endBuffer1);
 
     //sync to GPU
     std::cout << "Amplification factor before move assignment: " << std::get<0>(std::get<1>(edges).params.to_vector().front()) << std::endl;
@@ -94,13 +93,13 @@ int main() {
     std::get<1>(edges).params.copy_to_host(&newfactor);
     std::cout << "Amplification factor after issuing GPU sync: " << std::get<0>(std::get<1>(edges).params.to_vector().front()) << std::endl;
 
-    std::get<1>(edges).inverse(std::get<1>(nodes), std::get<2>(nodes));//BUG: Factor change is not synchronised from within kernel queue?
+    std::get<1>(edges).inverse(std::get<0>(nodes), std::get<1>(nodes));//BUG: Factor change is not synchronised from within kernel queue?
     std::vector<data_type> intermediateBuffer2(11);
-    std::get<1>(nodes).copy_to_host(intermediateBuffer2.data());
+    std::get<0>(nodes).copy_to_host(intermediateBuffer2.data());
     std::cout << "Jammed2: " << intermediateBuffer2 << std::endl;
-    std::get<1>(nodes) = std::move(intermediateBuffer2);
-    std::get<0>(edges).inverse(std::get<0>(nodes), std::get<1>(nodes));
+    std::get<0>(nodes) = std::move(intermediateBuffer2);
+    std::get<0>(edges).inverse(inputData, std::get<0>(nodes));
     std::vector<data_type> outputBuffer(12);
-    std::get<0>(nodes).copy_to_host(outputBuffer.data());
+    inputData.copy_to_host(outputBuffer.data());
     std::cout << "Sink: " << outputBuffer << std::endl;
 }
